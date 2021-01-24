a74a6537d48dcdfa857bd00f2b4d74d7c9aa4d46
Aryan Chavan <aryanchavan2003@gmail.com>
2021-01-24 22:41:52 +0530

    recieve and display messages

--- public/js/hall.js
+++ public/js/hall.js
@@ -2,6 +2,7 @@ const socket = io();
 
 const username = document.getElementById("username").innerText;
 const hall = document.getElementById("hall-name").innerText;
+const messageContainer = document.getElementById("messages-scroll");
 
 socket.emit("greeting", {
 	name: username,
@@ -18,5 +19,16 @@ document.getElementById("message-form").addEventListener("submit", (e) => {
 
 // recieving message
 socket.on("messageRecieve", (message) => {
-	console.log(message);
+	displayMessage(message);
 });
+
+// displaying message
+function displayMessage(message) {
+	const div = document.createElement("div");
+	div.classList.add("message");
+	div.innerHTML = `
+        <div class="sender">${message.username}</div>
+		<p>${message.message}</p>
+    `;
+	messageContainer.appendChild(div);
+}
